9f67e2c9c041b4bcbf9e074746f3cc7d24392668
Matthew Wildermuth <mgw32@pitt.edu>
2020-01-26 13:54:45 -0500

    Range testing basic update

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,9 +1,7 @@
 #include <Arduino.h>
 
-#define xbeepin1 1
-#define xbeepin2 2
-
-#define xbeeFreq 123456789
+#define ledStat 36
+#define ledStat2 38
 
 typedef enum {
    StartUp,
@@ -14,7 +12,15 @@ typedef enum {
 void setup()
 {
     Serial.begin(9600);//begin serial communication for GUI
+    delay(3000);
 
+    Serial1.begin(9600);
+    delay(3000);
+
+    pinMode(ledStat, OUTPUT);
+    pinMode(ledStat2, OUTPUT);
+
+    /*
     int counter;
     for(counter = 0; !Serial && counter < 2000; counter++){
         ;//Waiting for the Serial port to connect
@@ -24,48 +30,78 @@ void setup()
     else
         Serial.println("Serial port connected");
 
-    Serial2.begin(9600);//begin serial communication for xbees
+    Serial4.begin(9600);//begin serial communication for xbees
 
-    for(counter = 0; !Serial2 && counter < 2000; counter++){
-        ;//Waiting for the Serial2 port to connect
+    for(counter = 0; !Serial4 && counter < 2000; counter++){
+        ;//Waiting for the Serial4 port to connect
     }
     if(counter == 2000)
         Serial.println("Serial port couldn't connect");
     else
         Serial.println("Serial port connected");
-
+    */
 }
 
 void loop()
 {
-    while(Serial.available())
-    {
+        char logData;
+        //Serial.println("At main loop");
+        //Serial.println("Got here");
         //read and send the logged data to the GUI
-        if(Serial2.available() > 0)
+        while(Serial1.available())
         {
-            String logData = Serial2.readString();
+            //String logData = Serial1.readStringUntil('\n');
+            logData = Serial1.read();
+            Serial.print(logData);//sends transmitted data to the GUI
+        }
+
+        /*
+        if(!Serial1.available())
+        {
+            delay(1000);
+            digitalWrite(ledStat2, HIGH);
+            delay(1000);
+            digitalWrite(ledStat, LOW);
+        }
+        */
+        /*
+        if(Serial1.available() > 0)
+        {
+            //Serial.print("Serial 4 Available. Data: ");
+            //digitalWrite(ledStat, HIGH);
+            String logData = Serial1.readStringUntil('\n');
+            //String logData = Serial4.readString();
             Serial.println(logData);//sends transmitted data to the GUI
+            //delay(1000);
+            //digitalWrite(ledStat, LOW);
         }
         else
         {
-            Serial.println("No data available");
+            delay(1000);
+            digitalWrite(ledStat2, HIGH);
+            delay(1000);
+            digitalWrite(ledStat, LOW);
+            //Serial.println("No data available");
         }
+        */
+        //delay(1000);
 
+        /*
         //reads current state from the GUI, then executes some tasks accordingly
         uint8_t mode = Serial.read();
         State currState = (State) mode;
         String modeData = Serial.readString();
-        if(Serial2.availableForWrite())
+        if(Serial4.availableForWrite())
         {
             switch(currState){
             case Ready:{
-                //Serial2.println(mode + "," + modeData);
-                Serial2.printf("%d,%s\n", mode, modeData);//sending the mode to the xbee for transmission
+                //Serial4.println(mode + "," + modeData);
+                Serial4.printf("%d,%s\n", mode, modeData);//sending the mode to the xbee for transmission
                 break;
             }
             case StartUp:{
-                //Serial2.println(mode + "," + modeData);
-                Serial2.printf("%d,%s\n", mode, modeData);//sending the mode to the xbee for transmission
+                //Serial4.println(mode + "," + modeData);
+                Serial4.printf("%d,%s\n", mode, modeData);//sending the mode to the xbee for transmission
                 break;
             }
             case Flight:{
@@ -80,7 +116,6 @@ void loop()
         {
             Serial.println("Couldn't write to xbees");
         }
-
-    }
+        */
 
 }
